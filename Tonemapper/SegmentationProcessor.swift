669f4766d64a17fa312f787424e71b3f3ba45874
Philipp Waxweiler <philipp@waxweiler.de>
2018-03-14 15:07:55 +0100

    added execute function which waits until shader calculation has been completed

--- Tonemapper/SegmentationProcessor.swift
+++ Tonemapper/SegmentationProcessor.swift
@@ -45,4 +45,37 @@ final class SegmentationProcessor: MTKPComputer {
         computeEncoder.dispatchThreads(threadCount, threadsPerThreadgroup: descriptor.tgConfig.tgSize)
         computeEncoder.endEncoding()
     }
+    
+    public func execute(_ name:String, threads: MTLSize? = nil) {
+        guard
+            let descriptor = self.assets[name] as? MTKPComputePipelineStateDescriptor,
+            descriptor.state != nil,
+            let cmdBuffer = MTKPDevice.commandQueue.makeCommandBuffer(),
+            let computeEncoder = cmdBuffer.makeComputeCommandEncoder()
+            else {
+                fatalError()
+        }
+        guard (threads != nil) || (descriptor.textures != nil) else {
+            fatalError("The thread count is unknown. Pass it as an argument to the encode function.")
+        }
+        let threadCount = threads ?? descriptor.textures![0]!.size()
+        
+        computeEncoder.setComputePipelineState(descriptor.state!)
+        if let textures = descriptor.textures {
+            computeEncoder.setTextures(textures, range: 0..<textures.count)
+        }
+        if let buffers = descriptor.buffers {
+            computeEncoder.setBuffers(buffers, offsets: [Int](repeating: 0, count: buffers.count), range: 0..<buffers.count)
+        }
+        if let TGMemSize = descriptor.tgConfig.tgMemLength {
+            TGMemSize.enumerated().forEach({
+                computeEncoder.setThreadgroupMemoryLength($0.element, index: $0.offset)
+            })
+        }
+        computeEncoder.dispatchThreads(threadCount, threadsPerThreadgroup: descriptor.tgConfig.tgSize)
+        computeEncoder.endEncoding()
+        
+        cmdBuffer.commit()
+        cmdBuffer.waitUntilCompleted()
+    }
 }
