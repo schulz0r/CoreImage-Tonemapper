2c6118e75b73f296946fc4b0ab44e820c8fb4d74
Philipp Waxweiler <philipp@waxweiler.de>
2018-04-13 20:40:07 +0200

    changed storage mode of several buffers to private

--- Tonemapper/ShaderIOs/segmentationShaderIOP.swift
+++ Tonemapper/ShaderIOs/segmentationShaderIOP.swift
@@ -98,15 +98,14 @@ final class kMeansShaderIO: MTKPIOProvider {
         LabelTexture_descriptor.pixelFormat = .r8Uint
         
         var Means = Array<Float>(stride(from: 0, to: 1, by: 0.5)) + [1] // evenly distribute means over values
-        var K = Means.count
-        var bufferLen:uint = uint( K * Int(ceil(Float(grayInputTexture.width) / 16.0) * ceil(Float(grayInputTexture.height) / 16.0)) )
+        var K = uint(Means.count)
+        var bufferLen:uint = K * uint( Int(ceil(Float(grayInputTexture.width) / 16.0) * ceil(Float(grayInputTexture.height) / 16.0)) )
         
         guard
-            let Means_ = MTKPDevice.instance.makeBuffer(bytes: &Means, length: K * MemoryLayout<Float>.size, options: .cpuCacheModeWriteCombined),
-            let clusterSizes_ = MTKPDevice.instance.makeBuffer(length: 256 * MemoryLayout<uint>.size, options: .storageModePrivate),
-            let K_ = MTKPDevice.instance.makeBuffer(bytes: &K, length: MemoryLayout<Float>.size, options: .cpuCacheModeWriteCombined),
-            // buffer to store structs "clusterSum" (see .metal file). clusterSum consists of a uint and a half. Due to memory alignment, the half value takes 4 bytes, so here, we allocate 8 bytes (uint + float) for every clusterSum element
-            let Buffer_ = MTKPDevice.instance.makeBuffer(length: (MemoryLayout<Float>.size) * Int(bufferLen), options: .storageModeShared), // TODO: make it private
+            let Means_ = MTKPDevice.instance.makeBuffer(bytes: &Means, length: Means.count * MemoryLayout<Float>.size, options: .cpuCacheModeWriteCombined),
+            let clusterSizes_ = MTKPDevice.instance.makeBuffer(length: 256 * MemoryLayout<uint>.size, options: .cpuCacheModeWriteCombined),
+            let K_ = MTKPDevice.instance.makeBuffer(bytes: &K, length: MemoryLayout<uint>.size, options: .cpuCacheModeWriteCombined),
+            let Buffer_ = MTKPDevice.instance.makeBuffer(length: (MemoryLayout<Float>.size) * Int(bufferLen), options: .storageModePrivate),
             let BufferLen_ = MTKPDevice.instance.makeBuffer(bytes: &bufferLen, length: MemoryLayout<uint>.size, options: .cpuCacheModeWriteCombined),
             let LabelTexture = MTKPDevice.instance.makeTexture(descriptor: LabelTexture_descriptor)
         else {
