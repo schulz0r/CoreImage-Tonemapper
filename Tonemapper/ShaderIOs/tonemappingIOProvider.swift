8cd177845b017adf26469ba41d089bcbdc0b569b
Philipp Waxweiler <philipp@waxweiler.de>
2018-04-15 21:57:01 +0200

    added two kernels which will calculate the tonemap

--- Tonemapper/ShaderIOs/tonemappingIOProvider.swift
+++ Tonemapper/ShaderIOs/tonemappingIOProvider.swift
@@ -6,14 +6,36 @@
 //  Copyright © 2018 Philipp Waxweiler. All rights reserved.
 //
 
-import Metal
+import MetalKit
 import MetalKitPlus
 
 final class tonemappingIOProvider: MTKPIOProvider {
     private let inTexture : MTLTexture
+    private let tonemappingCurve, cubicSplineMatrix : MTLBuffer
     
-    init(linearHDR: MTLTexture, labels: MTLTexture){
+    init(linearHDR: MTLTexture, labels: MTLTexture, classes: Int){
+        let curveControlPointsCount = 12 // 8 control points + 4 at the start/end
+        var ControlPoints = [0] + stride(from: 0, to: 1, by: 1.0 / 8.0) + [1,1,1]
+        var matrix:[float4] = [float4(-1.0/6.0,0.5,-0.5,1.0/6.0),
+                               float4(0.5,-1,0,2.0/3.0),
+                               float4(-0.5,0.5,0.5,1.0/6.0),
+                               float4(1.0/6.0,0,0,0)]    // = float4x4
+        
+        guard
+            let tonemappingCurve_ = MTKPDevice.instance.makeBuffer(length: classes * curveControlPointsCount * MemoryLayout<Float>.size, options: .storageModeManaged),
+            let cubicSplineMatrix_ = MTKPDevice.instance.makeBuffer(bytes: &matrix, length: MemoryLayout<float4>.size * 4, options: .cpuCacheModeWriteCombined)
+            else {
+            fatalError()
+        }
+        
+        // start all control points with linear curves
+        (0..<classes).forEach{
+            memcpy(tonemappingCurve_.contents() + $0 * curveControlPointsCount * MemoryLayout<Float>.size, &ControlPoints, curveControlPointsCount * MemoryLayout<Float>.size)
+        }
+        
         self.inTexture = linearHDR
+        self.tonemappingCurve = tonemappingCurve_
+        self.cubicSplineMatrix = cubicSplineMatrix_
     }
     
     func fetchTextures() -> [MTLTexture?]? {
@@ -21,6 +43,6 @@ final class tonemappingIOProvider: MTKPIOProvider {
     }
     
     func fetchBuffers() -> [MTLBuffer]? {
-        return nil
+        return [tonemappingCurve, cubicSplineMatrix]
     }
 }
--- Tonemapper/tonemappingShader.metal
+++ Tonemapper/tonemappingShader.metal
@@ -9,3 +9,23 @@
 #include <metal_stdlib>
 using namespace metal;
 
+/* Tonemapping shaders
+ First, control points of a tonemapping curve will be calculted according to the paper below:
+ 
+ Bertalm, M., Hall, L., E, C. S. S., Ferradans, S., Provenzi, E., & Bertalm, M. (2009). An analysis of visual adaptation and contrast perception for a fast tone mapping operator.
+ 
+ This will be done for each class independently. TODO: If possible, let the cpu do this.
+ The second kernel will apply the tonemapping curve to each pixel w.r.t. its cluster.
+ */
+
+// this kernel calculates the tonemapping curve for some points only. The tonemapper will interpolate between these points.
+kernel void makeToneMappingCurve(device metal::array<float, 12> * tonemappingCurve [buffer(0)],
+                                 uint clusterIdx [[threadgroup_position_in_grid]]
+                                 uint controlPointIdx [[thread_index_in_threadgroup]]) {
+    
+    const float illumination = tonemappingCurve[clusterIdx][controlPointIdx];
+}
+
+kernel void tonemap() {
+    
+}
