d979fc112b6628b5b34a2bcdd9cec75e91f96d47
Philipp Waxweiler <philipp@waxweiler.de>
2018-05-05 22:26:48 +0200

    added test module for tonemapper

---
+++ TonemapperTests/TonemappingTest.swift
@@ -0,0 +1,9 @@
+//
+//  TonemappingTest.swift
+//  TonemapperTests
+//
+//  Created by Philipp Waxweiler on 05.05.18.
+//  Copyright © 2018 Philipp Waxweiler. All rights reserved.
+//
+
+import Foundation
